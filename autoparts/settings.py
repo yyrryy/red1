6dae0860f1681f09e8bc8187727cd2585430739d
Your Name <you@example.com>
2025-08-03 21:20:12 +0100

    'reer'

--- autoparts/settings.py
+++ autoparts/settings.py
@@ -167,7 +167,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "autoparts/static",
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
