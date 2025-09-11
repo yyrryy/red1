efa258363814ac78026d9db8844ecd7223b25b5d
Your Name <you@example.com>
2025-09-11 16:34:30 +0100

    'reer'

--- standalone.py
+++ standalone.py
@@ -1,21 +1,29 @@
 import os
 from threading import Thread
 from time import sleep
-import subprocess
-import sys
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close() 
+    return ip
 
 def runserver():
-    os.system('python manage.py runserver localhost:8000')
+    os.system('python manage.py runserver 0.0.0.0:8000')
 
 def lunchchrome():
-    # ensure the django server is up and running
-    sleep(2)
-    # get ipv4 address
-    os.system('start chrome http://localhost:8000')
-t1=Thread(target=runserver)
+    sleep(2)  # wait for server
+    ip = get_local_ip()
+    os.system(f'start chrome http://{ip}:8000')
 
-t2=Thread(target=lunchchrome)
+t1 = Thread(target=runserver)
+t2 = Thread(target=lunchchrome)
 
 t1.start()
 sleep(2)
 t2.start()
+
