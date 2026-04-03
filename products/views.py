bc0da36f80a1d8729a4be39f486a3cf043d17f78
Your Name <you@example.com>
2026-04-03 12:07:37 +0100

    'reer'

--- products/views.py
+++ products/views.py
@@ -10499,7 +10499,7 @@ def bonlivraisonprint(request, id):
     totalarticles = len(orderitems)
     reglements=PaymentClientbl.objects.filter(bons__in=[order])
     orderitems=list(orderitems)
-    orderitems=[orderitems[i:i+35] for i in range(0, len(orderitems), 35)]
+    orderitems=[orderitems[i:i+10] for i in range(0, len(orderitems), 10)]
     ctx={
         'title':f'Bon de livraison {order.bon_no}',
         'order':order,
